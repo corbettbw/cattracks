73d64610e72f9dab93e770f7a94ec26bfd904b55
Corbett Winningham <corbettbw@gmail.com>
2026-03-25 15:27:10 -0700

    reordering the mapping system to show the collection of sightings at the top of the cat profile

--- app/helpers/sightings_helper.rb
+++ app/helpers/sightings_helper.rb
@@ -1,2 +1,9 @@
 module SightingsHelper
+    def static_map_url(lat, lng, zoom: 15, width: 600, height: 200)
+        "https://staticmap.openstreetmap.de/staticmap.php?" \
+        "center=#{lat},#{lng}&" \
+        "zoom=#{zoom}&" \
+        "size=#{width}x#{height}&" \
+        "markers=#{lat},#{lng},red-pushpin"
+    end
 end
